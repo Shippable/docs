76b5c58f0a456f6f6f50bf867a76ccdd6fdd6c23
chetantarale <chetan_tarale@yahoo.com>
2017-04-10 18:34:08 +0530

    handles dynamic dispaly for left plane

--- theme/saas/static/js/expandCollapse.js
+++ theme/saas/static/js/expandCollapse.js
@@ -1,21 +1,33 @@
 $(document).ready(function () {
-  if ($.cookie('selectedMenuId')) {
-    var selectedId = $.cookie('selectedMenuId');
+  if (localStorage.getItem("selectedMenuId")) {
+    var selectedId = localStorage.getItem("selectedMenuId");
     $('#' + selectedId).addClass('active');
-    $.removeCookie('selectedMenuId');
+  }
+
+  if (localStorage.getItem("currentSection")) {
+    var selectedId = localStorage.getItem("currentSection");
+    $('ul.indiviualSection').addClass('hideSection');
+    $('ul#' + selectedId).removeClass('hideSection')
+  } else {
+    $('ul.indiviualSection').addClass('hideSection');
+    $('ul.indiviualSection').eq(0).removeClass('hideSection')
   }
 
   $('.shipNav-item').click(
     function () {
-      if ($(this).hasClass('active')) {
-        $(this).removeClass('active');
-      } else {
+      if (!$(this).hasClass('active')) {
         $('.shipNav-item').removeClass('active');
         $(this).addClass('active');
-        $.cookie('selectedMenuId', $(this).attr('id'),
-          {expires: 90, path: '/'}
-        );
+        if (typeof(Storage) !== "undefined") {
+          localStorage.setItem("selectedMenuId", $(this).attr('id'));
+        }
       }
+    }
+  );
+
+  $('.topPanel').click(
+    function () {
+      localStorage.setItem("currentSection", $(this).attr('id'));
     }
   );
 });
