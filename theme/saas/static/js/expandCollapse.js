9b03eb6c2ce1fbe30d00c6f065acd178655bcf6c
chetantarale <chetan_tarale@yahoo.com>
2017-04-12 12:46:44 +0530

    do not expand and contract on refresh

--- theme/saas/static/js/expandCollapse.js
+++ theme/saas/static/js/expandCollapse.js
@@ -1,10 +1,17 @@
 $(document).ready(
   function () {
+    $('.shipNav-item-nav').click(
+      function (e) {
+        e.stopPropagation();
+      }
+    );
     $('.shipNav-item').click(
       function () {
         if (!$(this).hasClass('active')) {
           $('.shipNav-item').removeClass('active');
           $(this).addClass('active');
+        } else {
+          $('.shipNav-item').removeClass('active');
         }
       }
     );
