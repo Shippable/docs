f5b1c42c535842e7a55b5cc5d5242baf7ddc2429
ambarish2012 <ambarish@shippable.com>
2017-05-08 19:48:04 -0700

    update docs

--- theme/saas/static/js/search.js
+++ theme/saas/static/js/search.js
@@ -30,6 +30,8 @@
             suggestion.link = suggestion.headings_link_map["page_main_heading"];
 
             var key =  null;
+            var partialMatchObject = null;
+
             function iterateObject(obj) {
                 for (var property in obj) {
                     if (obj.hasOwnProperty(property)) {
@@ -38,7 +40,16 @@
                                 && obj[property].matchLevel === "full") {
                                 key = property;
                                 return obj[property];
-                            }  else {
+                            } else {
+
+                                if ((typeof obj[property] == "object")
+                                    && obj[property].hasOwnProperty("matchLevel")
+                                    && (obj[property].matchLevel === "partial")
+                                    && (partialMatchObject == null)) {
+                                    key = property;
+                                    partialMatchObject = obj[property];
+                                }
+
                                 var val = iterateObject(obj[property]);
                                 if (val) {
                                     return val;
@@ -49,8 +60,25 @@
                 }
             }
 
-            var matchingObject = iterateObject(suggestion._snippetResult);
+            function findProperty(obj, prop) {
+                for (var property in obj) {
+                    if (obj.hasOwnProperty(property) && (typeof obj[property] == "object")) {
+                        if (property == prop) {
+                            return obj[property];
+                        } else {
+                            var val = findProperty(obj[property], prop);
+                            if (val) {
+                                return val;
+                            }
+                        }
+                    }
+                }
+            }
+
+            var matchingObject = iterateObject(suggestion._highlightResult) || partialMatchObject;
             if (matchingObject) {
+                matchingObject = findProperty(suggestion._snippetResult, key);
+
                 searchResult = matchingObject.value;
                 searchResult = searchResult.replace(/<em>/g, "<font color='DarkBlue'>");
                 searchResult = searchResult.replace(/<\/em>/g, "</font>");
@@ -68,9 +96,10 @@
 
             var sub_section = suggestion.page_main_title ? suggestion.page_main_title : suggestion.sub_section;
 
-            return searchResult + '<br>' + '<p>' + suggestion.main_section +
+            return '<p>' + suggestion.main_section +
                 '  <svg width="6" height="10" viewBox="0 0 6 10"><path fill="#6D86A5" d="M0 10l6-5-6-5" fill-rule="evenodd"/></svg>  '
-                + sub_section + '</p>';
+                + sub_section + '</p>'
+                + searchResult;
         }
       }
     }
